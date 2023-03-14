57c565621a2986254ccba73b25fef0355625cfef
MayorFalomo <mayorfalomo@gmail.com>
2023-03-14 19:29:20 +0100

    added local-storage theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import GlobalStyle from "./Globalstyles";
 import { AppContext } from "./helpers/Context";
@@ -9,8 +9,17 @@ function App() {
   //Global States
   const [countries, setCountries] = useState<any>([]);
   const [countryInfo, setCountryInfo] = useState([]);
-  const [theme, setTheme] = useState<string>("light");
   const [searchText, setSearchText] = useState<string>("");
+
+  const getThemeInStorage = () => {
+    return JSON.parse(localStorage.getItem("theme") || '{}');
+  }
+
+  const [theme, setTheme] = useState<any>(getThemeInStorage());
+  
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme))
+  },[theme])
 
   return (
     <AppContext.Provider
